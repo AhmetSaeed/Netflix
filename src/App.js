eec2996fb9f940e1bba314f0e2b6c8c5e2e0b122
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-05-29 19:34:46 +0300

    I have done but still i meet an error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import Header from "./Components/Header";
 import Search from "./Components/Search";
 import MovieCard from "./Components/MovieCard";
+import { useState } from "react"
+import { useEffect } from "react";
 function App() {
- return (
-  <div classNameName="App">
-   <Header />
-   <div className="main">
-    <Search />
-    <div className="movies-section">
-     {/* <!-- one card --> */}
+    // const [name, setName] = useState()
+    // const [img, setImg] = useState()
+    // const [search, setS] = useState()
+    let [data, setData] = useState()
+    let [searchHolder, setSearchHolder] = useState("")
 
-     {/* <!-- one card --> */}
-    </div>
-   </div>
-  </div>
- );
+
+    function searchF(valueHolder) {
+        setSearchHolder(valueHolder)
+    }
+
+
+    fetch(` https://api.tvmaze.com/search/shows?q=${searchHolder}`)
+        .then((res) => {
+            return res.json()
+        }).then((data) => {
+            const filteredData = data.map(item => item.show)
+            setData(filteredData)
+
+        })
+        .catch(() => {
+            console.log("error we can't find that Movie for now")
+        })
+
+
+
+    if (!data) {
+        return <div >loading...</div>
+    }
+
+
+    return (
+        <div classNameName="App">
+            <Header />
+            <div className="main">
+                <Search test={searchF} />
+                <div className="movies-section">
+                    {/* <!-- one card --> */}
+                    {data.map((check) => <MovieCard item={check} />)}
+                    {/* <!-- one card --> */}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
